f502bd6922d884f31644c7666f971f3e8f300103
Christian Bracamontes <brac24@live.com>
2017-08-17 16:53:17 -0700

    Got first Nunit test runnning and set up and passing

--- FeatureCreationPresenterTests.cs
+++ FeatureCreationPresenterTests.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+
 
 namespace Feature_Inspection.UnitTests
 {
+    [TestFixture]
     class FeatureCreationPresenterTests
     {
+        [Test]
+        public void Ctor_PassingInModelAndView_ObjectsNotNull()
+        {
+            var view = new FeatureCreationTableMock();
+            var model = new FeatureCreationModelMock();
+
+            var presenter = new FeatureCreationPresenter(view,model);
+
+            Assert.That(presenter.ViewExists());
+            
+        }
+        
     }
 }
