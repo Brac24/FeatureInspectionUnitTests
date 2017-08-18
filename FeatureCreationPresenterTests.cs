445c7264a9ce7e033e92aaee8e19d372a700fcd6
Christian Bracamontes <brac24@live.com>
2017-08-18 07:12:51 -0700

    Tried some things

--- FeatureCreationPresenterTests.cs
+++ FeatureCreationPresenterTests.cs
@@ -11,19 +11,49 @@ using NUnit.Framework;
 namespace Feature_Inspection.UnitTests
 {
     [TestFixture]
+    class FeatureCreationViewTests
+    {
+        /*
+        [Test]
+        public void Event_TestingEditClicked_EventFired()
+        {
+            //Arrange
+            var view = new FeatureCreationTableMock();
+
+            var wasFired = false;
+
+            view.EditClicked += (o, e) => wasFired = true;
+
+            
+            Assert.That(wasFired, Is.True);
+
+
+        }
+        */
+    }
+
+
+    [TestFixture]
     class FeatureCreationPresenterTests
     {
         [Test]
         public void Ctor_PassingInModelAndView_ObjectsNotNull()
         {
+            //Arrange
             var view = new FeatureCreationTableMock();
             var model = new FeatureCreationModelMock();
 
+            //Act
             var presenter = new FeatureCreationPresenter(view,model);
-
-            Assert.That(presenter.ViewExists());
             
+
+            //Assert
+            Assert.That(presenter, Is.Not.Null);
+                       
         }
+
+       
+        
         
     }
 }
